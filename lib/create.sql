b06067f06cefa606af82963ad294327e18b4d871
Laura Darlak <laura@polyporedesign.com>
2018-10-07 20:34:43 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,10 +3,10 @@
   start_date INTEGER, end_date INTEGER
 );
 
-​CREATE TABLE user (
-  id INTEGER PRIMARY KEY, name TEXT, age INTEGER
-);
-
-CREATE TABLE pledge (
-  id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER
-);
+-- ​CREATE TABLE user (
+--   id INTEGER PRIMARY KEY, name TEXT, age INTEGER
+-- );
+--
+-- CREATE TABLE pledge (
+--   id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER
+-- );
