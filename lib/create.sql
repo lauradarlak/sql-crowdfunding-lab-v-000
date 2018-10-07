95c2d1ace3ed6eb145faedd1b70cd36cb49381ed
Laura Darlak <laura@polyporedesign.com>
2018-10-07 20:34:01 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,12 @@
+​CREATE TABLE project (
+  id INTEGER PRIMARY KEY, title TEXT, funding_goal INTEGER,
+  start_date INTEGER, end_date INTEGER
+);
+
+​CREATE TABLE user (
+  id INTEGER PRIMARY KEY, name TEXT, age INTEGER
+);
+
+CREATE TABLE pledge (
+  id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER
+);
